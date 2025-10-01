1090c9ec7e14b98e82cb6e07b18f783254db8983
Dolev Hadar <dolevc2@gmail.com>
2025-10-01 13:50:07 +0300

    chore

--- grant-revoke.py
+++ grant-revoke.py
@@ -1,11 +1,10 @@
 import os
 from pathlib import Path
 
-from insiders import GitHub, Polar, update_sponsors_file, update_numbers_file
+from insiders import GitHub, update_sponsors_file, update_numbers_file
 
 
 GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
-POLAR_TOKEN = os.environ["POLAR_TOKEN"]
 TEAM = "dlvhdr-insiders/insiders"
 MIN_AMOUNT = 10
 
@@ -19,13 +18,12 @@ include_users = {
 }
 
 exclude_users = {
-    "medecau",  # Doesn't want to join the team.
 }
 
 
 def main():
-    with GitHub(GITHUB_TOKEN) as github, Polar(POLAR_TOKEN) as polar:
-        sponsors = github.get_sponsors() + polar.get_sponsors()
+    with GitHub(GITHUB_TOKEN) as github:
+        sponsors = github.get_sponsors()
         github.consolidate_beneficiaries(sponsors, beneficiaries)  # type: ignore[arg-type]
         github.sync_team(
             TEAM,
